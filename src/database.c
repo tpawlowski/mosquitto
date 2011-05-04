dde4682228c50c2b55500297d9e21f983b21a0e7
Roger Light <roger@atchoo.org>
2011-05-04 22:57:04 +0100

    Allow source_id to be null here.

--- src/database.c
+++ src/database.c
@@ -432,7 +432,7 @@ int mqtt3_db_messages_easy_queue(mosquitto_db *db, mqtt3_context *context, const
 	if(context){
 		source_id = context->core.id;
 	}else{
-		source_id = "";
+		source_id = NULL;
 	}
 	if(mqtt3_db_message_store(db, source_id, 0, topic, qos, payloadlen, payload, retain, &stored, 0)) return 1;
 
@@ -447,7 +447,7 @@ int mqtt3_db_messages_queue(mosquitto_db *db, const char *source_id, const char 
 	assert(stored);
 
 	/* Find all clients that subscribe to topic and put messages into the db for them. */
-	if(!source_id || !topic) return MOSQ_ERR_INVAL;
+	if(!topic) return MOSQ_ERR_INVAL;
 
 	mqtt3_sub_search(db, &db->subs, source_id, topic, qos, retain, stored);
 	return rc;
