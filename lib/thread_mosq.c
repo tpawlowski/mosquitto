815a8857b79e67aa3a0f0e17e4284f6fdf85d57d
Roger Light <roger@atchoo.org>
2011-08-03 15:28:46 +0100

    Add missing file.

---
+++ lib/thread_mosq.c
@@ -0,0 +1,80 @@
+/*
+Copyright (c) 2011 Roger Light <roger@atchoo.org>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. Neither the name of mosquitto nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <config.h>
+
+#include <unistd.h>
+
+#include <mosquitto_internal.h>
+#include <thread_mosq.h>
+
+void *_mosquitto_thread_main(void *obj);
+
+int mosquitto_loop_start(struct mosquitto *mosq)
+{
+	if(!mosq) return MOSQ_ERR_INVAL;
+
+	MOSQUITTO_THREAD_CREATE(&mosq->thread_id, _mosquitto_thread_main, mosq);
+	return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_loop_stop(struct mosquitto *mosq)
+{
+	if(!mosq) return MOSQ_ERR_INVAL;
+	
+	pthread_join(mosq->thread_id, NULL);
+
+	return MOSQ_ERR_SUCCESS;
+}
+
+void *_mosquitto_thread_main(void *obj)
+{
+	struct mosquitto *mosq = obj;
+	int run = 1;
+	int rc;
+
+	if(!mosq) return NULL;
+
+	while(run){
+		do{
+			rc = mosquitto_loop(mosq, -1);
+		}while(rc == MOSQ_ERR_SUCCESS);
+		MOSQUITTO_MUTEX_LOCK(&mosq->state_mutex);
+		if(mosq->core.state == mosq_cs_disconnecting){
+			run = 0;
+			MOSQUITTO_MUTEX_UNLOCK(&mosq->state_mutex);
+		}else{
+			MOSQUITTO_MUTEX_UNLOCK(&mosq->state_mutex);
+			sleep(1);
+			mosquitto_reconnect(mosq);
+		}
+	}
+	return obj;
+}
+
