b68f39f3ee5c06beaaa3beaf0fa309bdf2e2cc34
Roger Light <roger@atchoo.org>
2012-03-11 17:15:50 +0000

    Init/cleanup mysql and mosquitto handles.

--- client/mysql_log.c
+++ client/mysql_log.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 
+#ifndef WIN32
+#  include <unistd.h>
+#else
+#  include <process.h>
+#  define snprintf sprintf_s
+#endif
+
 #include <mosquitto.h>
 #include <mysql/mysql.h>
 
-MYSQL *mysql_connection;
-
 int main(int argc, char *argv[])
 {
-	mysql_library_init(0, NULL, NULL);
+	MYSQL *connection;
+	my_bool reconnect = true;
+	char clientid[50];
+	struct mosquitto *mosq;
 
+	mysql_library_init(0, NULL, NULL);
+	mosquitto_lib_init();
+
+	connection = mysql_init(NULL);
+
+	if(connection){
+		mysql_options(connection, MYSQL_OPT_RECONNECT, &reconnect);
+
+		snprintf(clientid, 50, "mysql_log_%d", getpid());
+		mosq = mosquitto_new(clientid, connection);
+		if(mosq){
+			mosquitto_destroy(mosq);
+		}
+
+		mysql_close(connection);
+	}
 
 	mysql_library_end();
+	mosquitto_lib_cleanup();
+
 	return 0;
 }
 
