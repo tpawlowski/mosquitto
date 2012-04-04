4bdcedff962de242a0a2dd388336748e88af569f
Roger Light <roger@atchoo.org>
2012-04-04 20:12:07 +0100

    Add broker tests for invalid client id.

---
+++ test/broker/01-connect-invalid-id-0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import socket
+from struct import *
+
+connect_packet = pack('bbbb6sbbbbbb', 16, 12+2,0,6,"MQIsdp",3,2,0,10,0,0)
+connack_packet = pack('bbbb', 32, 2, 0, 2);
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 1888))
+sock.send(connect_packet)
+connack_recvd = sock.recv(256)
+sock.close()
+
+if connack_recvd != connack_packet:
+	(cmd, rl, resv, rc) = unpack('bbbb', connack_recvd)
+	print "FAIL: Expected 32,2,0,3 got " + str(cmd) + "," + str(rl) + "," + str(resv) + "," + str(rc)
+	exit(1)
+
