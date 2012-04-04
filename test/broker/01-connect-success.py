a60804ce7206dce0f9b9e31e4636250815241e19
Roger Light <roger@atchoo.org>
2012-04-04 12:01:59 +0100

    Add broker test for successful connect.

---
+++ test/broker/01-connect-success.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+from struct import *
+
+connect_packet = pack('bbbb6sbbbbbb20s', 16, 12+2+20,0,6,"MQIsdp",3,2,0,10,0,20,"connect-success-test")
+connack_packet = pack('bbbb', 32, 2, 0, 0);
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 1888))
+sock.send(connect_packet)
+connack_recvd = sock.recv(256)
+sock.close()
+
+if connack_recvd != connack_packet:
+	exit(1)
+
