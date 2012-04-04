b8c2db5c216d3346a85f5ed2851786e7090d9c18
Roger Light <roger@atchoo.org>
2012-04-04 23:50:09 +0100

    Should use B in pack() for unsigned not signed char.

--- test/broker/01-connect-success.py
+++ test/broker/01-connect-success.py
@@ -3,8 +3,8 @@
 import socket
 from struct import *
 
-connect_packet = pack('bbbb6sbbbbbb20s', 16, 12+2+20,0,6,"MQIsdp",3,2,0,10,0,20,"connect-success-test")
-connack_packet = pack('bbbb', 32, 2, 0, 0);
+connect_packet = pack('BBBB6sBBBBBB20s', 16, 12+2+20,0,6,"MQIsdp",3,2,0,10,0,20,"connect-success-test")
+connack_packet = pack('BBBB', 32, 2, 0, 0);
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("localhost", 1888))
@@ -13,7 +13,7 @@ connack_recvd = sock.recv(256)
 sock.close()
 
 if connack_recvd != connack_packet:
-	(cmd, rl, resv, rc) = unpack('bbbb', connack_recvd)
+	(cmd, rl, resv, rc) = unpack('BBBB', connack_recvd)
 	print "FAIL: Expected 32,2,0,0 got " + str(cmd) + "," + str(rl) + "," + str(resv) + "," + str(rc)
 	exit(1)
 
