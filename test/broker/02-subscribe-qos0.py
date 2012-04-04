ed6c0fb385935ba0ae13755a8196b205270711bd
Roger Light <roger@atchoo.org>
2012-04-04 23:50:37 +0100

    Add tests for SUBSCRIBE/SUBACK for all QoS.

---
+++ test/broker/02-subscribe-qos0.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import socket
+from struct import *
+
+connect_packet = pack('BBBB6sBBBBBB19s', 16, 12+2+19,0,6,"MQIsdp",3,2,0,60,0,19,"subscribe-qos0-test")
+connack_packet = pack('BBBB', 32, 2, 0, 0);
+
+subscribe_packet = pack('BBBBBB9sB', 130, 2+2+9+1, 0, 1, 0, 9, "qos0/test", 0)
+suback_packet = pack('BBBBB', 144, 2+1, 0, 1, 0)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 1888))
+sock.send(connect_packet)
+connack_recvd = sock.recv(256)
+
+if connack_recvd != connack_packet:
+	print "FAIL: Connect failed."
+	exit(1)
+
+sock.send(subscribe_packet)
+suback_recvd = sock.recv(256)
+sock.close()
+
+if suback_recvd != suback_packet:
+	(cmd, rl, midh, midl, qos) = unpack('BBBBB', suback_recvd)
+	print "FAIL: Expected 144,3,0,1,0 got " + str(cmd) + "," + str(rl) + "," + str(midh) + "," + str(midl) + "," + str(qos)
