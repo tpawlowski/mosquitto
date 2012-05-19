13f73e43343a823ba52232ef99dd28196ba8bbaf
Roger Light <roger@atchoo.org>
2012-05-19 14:45:24 +0100

    Add client test for broker to client qos 1 publishing.

---
+++ test/lib/c/03-publish-b2c-qos1.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mosquitto.h>
+
+static int run = -1;
+
+void on_connect(struct mosquitto *mosq, void *obj, int rc)
+{
+	if(rc){
+		exit(1);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int rc;
+	struct mosquitto *mosq;
+
+	mosquitto_lib_init();
+
+	mosq = mosquitto_new("publish-qos1-test", true, NULL);
+	mosquitto_connect_callback_set(mosq, on_connect);
+	mosquitto_message_retry_set(mosq, 5);
+
+	rc = mosquitto_connect(mosq, "localhost", 1888, 60);
+
+	while(run == -1){
+		mosquitto_loop(mosq, 300);
+	}
+
+	mosquitto_lib_cleanup();
+	return run;
+}
