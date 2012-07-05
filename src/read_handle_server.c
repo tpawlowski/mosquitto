278af62f1e143a9b31e14a5e96d33ca9d0f56dbf
Roger Light <roger@atchoo.org>
2012-07-05 10:45:48 +0100

    Fix incorrect anonymous user check.

--- src/read_handle_server.c
+++ src/read_handle_server.c
@@ -203,24 +203,25 @@ int mqtt3_handle_connect(mosquitto_db *db, struct mosquitto *context)
 			return MOSQ_ERR_SUCCESS;
 		}
 		name_entry = X509_NAME_get_entry(name, i);
-		printf("CN: %s\n", ASN1_STRING_data(name_entry->value));
 		context->username = _mosquitto_strdup((char *)ASN1_STRING_data(name_entry->value));
 		if(!context->username){
 			_mosquitto_free(client_id);
 			return MOSQ_ERR_NOMEM;
 		}
-	}else if(username_flag){
-		rc = mosquitto_unpwd_check(db, username, password);
-		context->username = username;
-		context->password = password;
-		if(rc == MOSQ_ERR_AUTH){
-			_mosquitto_send_connack(context, CONNACK_REFUSED_BAD_USERNAME_PASSWORD);
-			mqtt3_context_disconnect(db, context);
+	}else{
+		if(username_flag){
+			rc = mosquitto_unpwd_check(db, username, password);
+			context->username = username;
+			context->password = password;
+			if(rc == MOSQ_ERR_AUTH){
+				_mosquitto_send_connack(context, CONNACK_REFUSED_BAD_USERNAME_PASSWORD);
+				mqtt3_context_disconnect(db, context);
 			_mosquitto_free(client_id);
-			return MOSQ_ERR_SUCCESS;
-		}else if(rc == MOSQ_ERR_INVAL){
-			_mosquitto_free(client_id);
-			return MOSQ_ERR_INVAL;
+				return MOSQ_ERR_SUCCESS;
+			}else if(rc == MOSQ_ERR_INVAL){
+				_mosquitto_free(client_id);
+				return MOSQ_ERR_INVAL;
+			}
 		}
 
 		if(!username_flag && db->config->allow_anonymous == false){
