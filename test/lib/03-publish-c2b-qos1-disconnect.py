4961c9f08c78a085971efdef909631864cbc81ed
Roger Light <roger@atchoo.org>
2012-08-02 12:38:50 +0100

    Add lib test for resending qos1 messages on reconnect.

---
+++ test/lib/03-publish-c2b-qos1-disconnect.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+# Test whether a client sends a correct PUBLISH to a topic with QoS 1, then responds correctly to a disconnect.
+
+import os
+import subprocess
+import socket
+import sys
+import time
+from struct import *
+
+import mqtt_decode
+
+rc = 1
+keepalive = 60
+connect_packet = pack('!BBH6sBBHH17s', 16, 12+2+17,6,"MQIsdp",3,2,keepalive,17,"publish-qos1-test")
+connack_packet = pack('!BBBB', 32, 2, 0, 0);
+
+disconnect_packet = pack('!BB', 224, 0)
+
+mid = 1
+publish_packet = pack('!BBH13sH7s', 48+2, 2+13+2+7, 13, "pub/qos1/test", mid, "message")
+publish_packet_dup = pack('!BBH13sH7s', 48+8+2, 2+13+2+7, 13, "pub/qos1/test", mid, "message")
+puback_packet = pack('!BBH', 64, 2, mid)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.settimeout(10)
+sock.bind(('', 1888))
+sock.listen(5)
+
+client_args = sys.argv[1:]
+env = dict(os.environ)
+env['LD_LIBRARY_PATH'] = '../../lib:../../lib/cpp'
+try:
+    pp = env['PYTHONPATH']
+except KeyError:
+    pp = ''
+env['PYTHONPATH'] = '../../lib/python:'+pp
+
+client = subprocess.Popen(client_args, env=env)
+
+try:
+    (conn, address) = sock.accept()
+    conn.settimeout(5)
+    connect_recvd = conn.recv(256)
+
+    if connect_recvd != connect_packet:
+        print("FAIL: Received incorrect connect.")
+        print("Received: "+connect_recvd+" length="+str(len(connect_recvd)))
+        print("Expected: "+connect_packet+" length="+str(len(connect_packet)))
+    else:
+        conn.send(connack_packet)
+        publish_recvd = conn.recv(256)
+
+        if publish_recvd != publish_packet:
+            print("FAIL: Received incorrect publish.")
+            print("Received: "+publish_recvd+" length="+str(len(publish_recvd)))
+            print("Expected: "+publish_packet+" length="+str(len(publish_packet)))
+        else:
+            # Disconnect client. It should reconnect.
+            conn.close()
+
+
+            (conn, address) = sock.accept()
+            conn.settimeout(5)
+            connect_recvd = conn.recv(256)
+
+            if connect_recvd != connect_packet:
+                print("FAIL: Received incorrect connect.")
+                print("Received: "+connect_recvd+" length="+str(len(connect_recvd)))
+                print("Expected: "+connect_packet+" length="+str(len(connect_packet)))
+            else:
+                conn.send(connack_packet)
+                publish_recvd = conn.recv(256)
+
+                if publish_recvd != publish_packet_dup:
+                    print("FAIL: Received incorrect retried publish.")
+                    print("Received: "+mqtt_decode.mqtt_decode(publish_recvd))
+                    print("Expected: "+mqtt_decode.mqtt_decode(publish_packet_dup))
+                else:
+                    conn.send(puback_packet)
+                    disconnect_recvd = conn.recv(256)
+
+                    if disconnect_recvd != disconnect_packet:
+                        print("FAIL: Received incorrect disconnect.")
+                        (cmd, rl) = unpack('!BB', disconnect_recvd)
+                        print("Received: "+str(cmd)+", " + str(rl))
+                        print("Expected: 224, 0")
+                    else:
+                        rc = 0
+
+    conn.close()
+finally:
+    client.terminate()
+    client.wait()
+    sock.close()
+
+exit(rc)
---
+++ test/lib/mqtt_decode.py
@@ -0,0 +1,129 @@
+import struct
+
+def remaining_length(packet):
+    l = min(5, len(packet))
+    all_bytes = struct.unpack("!"+"B"*l, packet[:l])
+    mult = 1
+    rl = 0
+    for i in range(1,l-1):
+        byte = all_bytes[i]
+
+        rl += (byte & 127) * mult
+        mult *= 128
+        if byte & 128 == 0:
+            packet = packet[i+1:]
+            break
+
+    return (packet, rl)
+
+
+def mqtt_decode(packet):
+    if len(packet) == 0:
+        return ""
+
+    packet0 = struct.unpack("!B", packet[0])
+    packet0 = packet0[0]
+    cmd = packet0 & 0xF0
+    if cmd == 0x00:
+        # Reserved
+        return "0x00"
+    elif cmd == 0x10:
+        # CONNECT
+        return "FIXME"
+    elif cmd == 0x20:
+        # CONNACK
+        (cmd, rl, resv, rc) = unpack('!BBBB', packet)
+        return "CONNACK, rl="+str(rl)+", res="+str(resv)+", rc="+str(rc)
+    elif cmd == 0x30:
+        # PUBLISH
+        dup = (packet0 & 0x08)>>3
+        qos = (packet0 & 0x06)>>1
+        retain = (packet0 & 0x01)
+        (packet, rl) = remaining_length(packet)
+        pack_format = "!H" + str(len(packet)-2) + 's'
+        (tlen, packet) = struct.unpack(pack_format, packet)
+        pack_format = "!" + str(tlen)+'s' + str(len(packet)-tlen) + 's'
+        (topic, packet) = struct.unpack(pack_format, packet)
+        s = "PUBLISH, rl="+str(rl)+", qos="+str(qos)+", retain="+str(retain)+", dup="+str(dup)
+        if qos > 0:
+            pack_format = "!H" + str(len(packet)-2) + 's'
+            (mid, packet) = struct.unpack(pack_format, packet)
+            s = s + ", mid="+str(mid)
+
+        s = s + ", payload="+packet
+        return s
+    elif cmd == 0x40:
+        # PUBACK
+        (cmd, rl, mid) = unpack('!BBH', packet)
+        return "PUBACK, rl="+str(rl)+", mid="+str(mid)
+    elif cmd == 0x50:
+        # PUBREC
+        (cmd, rl, mid) = unpack('!BBH', packet)
+        return "PUBREC, rl="+str(rl)+", mid="+str(mid)
+    elif cmd == 0x60:
+        # PUBREL
+        (cmd, rl, mid) = unpack('!BBH', packet)
+        return "PUBREL, rl="+str(rl)+", mid="+str(mid)
+    elif cmd == 0x70:
+        # PUBCOMP
+        (cmd, rl, mid) = unpack('!BBH', packet)
+        return "PUBCOMP, rl="+str(rl)+", mid="+str(mid)
+    elif cmd == 0x80:
+        # SUBSCRIBE
+        (packet, rl) = remaining_length(packet)
+        pack_format = "!H" + str(len(packet)-2) + 's'
+        (mid, packet) = struct.unpack(pack_format, packet)
+        s = "SUBSCRIBE, rl="+str(rl)+", mid="+str(mid)
+        topic_index = 0
+        while len(packet) > 0:
+            pack_format = "!H" + str(len(packet)-2) + 's'
+            (tlen, packet) = struct.unpack(pack_format, packet)
+            pack_format = "!" + str(tlen)+'sB' + str(len(packet)-tlen) + 's'
+            (topic, qos, packet) = struct.unpack(pack_format, packet)
+            s = s + ", topic"+str(topic_index)+"="+topic+","+str(qos)
+        return s
+    elif cmd == 0x90:
+        # SUBACK
+        (packet, rl) = remaining_length(packet)
+        pack_format = "!H" + str(len(packet)-2) + 's'
+        (mid, packet) = struct.unpack(pack_format, packet)
+        pack_format = "!" + "B"*len(packet)
+        granted_qos = struct.unpack(pack_format, packet)
+        
+        s = "SUBACK, rl="+str(rl)+", mid="+str(mid)+", granted_qos="+str(granted_qos[0])
+        for i in range(1, len(granted_qos)-1):
+            s = s+", "+str(granted_qos[i])
+        return s
+    elif cmd == 0xA0:
+        # UNSUBSCRIBE
+        (packet, rl) = remaining_length(packet)
+        pack_format = "!H" + str(len(packet)-2) + 's'
+        (mid, packet) = struct.unpack(pack_format, packet)
+        s = "UNSUBSCRIBE, rl="+str(rl)+", mid="+str(mid)
+        topic_index = 0
+        while len(packet) > 0:
+            pack_format = "!H" + str(len(packet)-2) + 's'
+            (tlen, packet) = struct.unpack(pack_format, packet)
+            pack_format = "!" + str(tlen)+'s' + str(len(packet)-tlen) + 's'
+            (topic, packet) = struct.unpack(pack_format, packet)
+            s = s + ", topic"+str(topic_index)+"="+topic
+        return s
+    elif cmd == 0xB0:
+        # UNSUBACK
+        (cmd, rl, mid) = unpack('!BBH', packet)
+        return "UNSUBACK, rl="+str(rl)+", mid="+str(mid)
+    elif cmd == 0xC0:
+        # PINGREQ
+        (cmd, rl) = unpack('!BB', packet)
+        return "PINGREQ, rl="+str(rl)
+    elif cmd == 0xD0:
+        # PINGRESP
+        (cmd, rl) = unpack('!BB', packet)
+        return "PINGRESP, rl="+str(rl)
+    elif cmd == 0xE0:
+        # DISCONNECT
+        (cmd, rl) = unpack('!BB', packet)
+        return "DISCONNECT, rl="+str(rl)
+    elif cmd == 0xF0:
+        # Reserved
+        return "0xF0"
