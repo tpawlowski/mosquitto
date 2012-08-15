3d29cad334446613180e0279cb1197fab1f77764
Roger Light <roger@atchoo.org>
2012-08-15 11:47:24 +0100

    Add Python classifiers for PyPi.

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -9,5 +9,20 @@ setup(name='mosquitto',
 	url='http://mosquitto.org/',
 	download_url='http://mosquitto.org/files/',
 	license='BSD License',
-	py_modules=['mosquitto']
+	py_modules=['mosquitto'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications',
+        'Topic :: Internet',
+        ]
 	)
