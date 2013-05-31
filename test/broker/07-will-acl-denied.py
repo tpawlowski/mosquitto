e423536540f7806d56e2f27adfdb673068f85b46
Roger Light <roger@atchoo.org>
2013-05-31 21:19:13 +0100

    Update Will acl denied test.

--- test/broker/07-will-acl-denied.py
+++ test/broker/07-will-acl-denied.py
@@ -15,6 +15,9 @@ import mosq_test
 rc = 1
 mid = 53
 keepalive = 60
+connect_packet_ok = mosq_test.gen_connect("will-acl-test", keepalive=keepalive, will_topic="ok", will_payload="should be ok")
+connack_packet_ok = mosq_test.gen_connack(rc=0)
+
 connect_packet = mosq_test.gen_connect("will-acl-test", keepalive=keepalive, will_topic="will/acl/test", will_payload="should be denied")
 connack_packet = mosq_test.gen_connack(rc=5)
 
@@ -23,15 +26,25 @@ broker = subprocess.Popen(['../../src/mosquitto', '-c', '07-will-acl-denied.conf
 try:
     time.sleep(0.5)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(30)
-    sock.connect(("localhost", 1888))
-    sock.send(connect_packet)
+    sock_ok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_ok.settimeout(5)
+    sock_ok.connect(("localhost", 1888))
+    sock_ok.send(connect_packet_ok)
+    sock = None
 
-    if mosq_test.expect_packet(sock, "connack", connack_packet):
-        rc = 0
+    if mosq_test.expect_packet(sock_ok, "connack_ok", connack_packet_ok):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(("localhost", 1888))
+        sock.send(connect_packet)
 
-    sock.close()
+        if mosq_test.expect_packet(sock, "connack", connack_packet):
+            rc = 0
+
+    sock_ok.close()
+    if sock:
+        sock.close()
+
 finally:
     broker.terminate()
     broker.wait()
