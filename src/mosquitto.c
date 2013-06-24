687d2b8ce79a5cc61fce0044e64cf2eb01edded9
Roger Light <roger@atchoo.org>
2013-06-24 22:03:49 +0100

    Generate changeset id on dist rather than with keyword extension.

--- src/mosquitto.c
+++ src/mosquitto.c
@@ -233,8 +233,10 @@ int main(int argc, char *argv[])
 	mqtt3_db_messages_easy_queue(&int_db, NULL, "$SYS/broker/version", 2, strlen(buf), buf, 1);
 	snprintf(buf, 1024, "%s", TIMESTAMP);
 	mqtt3_db_messages_easy_queue(&int_db, NULL, "$SYS/broker/timestamp", 2, strlen(buf), buf, 1);
-	snprintf(buf, 1024, "%s", "$Revision$"); // Requires hg keyword extension.
+#ifdef CHANGESET
+	snprintf(buf, 1024, "%s", CHANGESET);
 	mqtt3_db_messages_easy_queue(&int_db, NULL, "$SYS/broker/changeset", 2, strlen(buf), buf, 1);
+#endif
 
 	listener_max = -1;
 	listensock_index = 0;
