94033b60f88f2f1a489c91961dd6aa851041694f
Roger Light <roger@atchoo.org>
2013-09-02 13:55:12 +0100

    The broker no longer ignores the auth_plugin_init() return value. Closes bug #1215084.
    Thanks to Jan-Piet Mens.

--- src/security.c
+++ src/security.c
@@ -51,6 +51,7 @@ int mosquitto_security_module_init(struct mosquitto_db *db)
 	void *lib;
 	int (*plugin_version)(void) = NULL;
 	int version;
+	int rc;
 	if(db->config->auth_plugin){
 		lib = LIB_LOAD(db->config->auth_plugin);
 		if(!lib){
@@ -126,7 +127,12 @@ int mosquitto_security_module_init(struct mosquitto_db *db)
 		db->auth_plugin.lib = lib;
 		db->auth_plugin.user_data = NULL;
 		if(db->auth_plugin.plugin_init){
-			db->auth_plugin.plugin_init(&db->auth_plugin.user_data, db->config->auth_options, db->config->auth_option_count);
+			rc = db->auth_plugin.plugin_init(&db->auth_plugin.user_data, db->config->auth_options, db->config->auth_option_count);
+			if(rc){
+				_mosquitto_log_printf(NULL, MOSQ_LOG_ERR,
+						"Error: Authentication plugin returned %d when initialising.", rc);
+			}
+			return rc;
 		}
 	}else{
 		db->auth_plugin.lib = NULL;
