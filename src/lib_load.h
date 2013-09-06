7ccbe27ac4e58e799666d35893bd05e1e5512f2e
Roger Light <roger@atchoo.org>
2013-09-06 21:41:09 +0100

    Use RTLD_GLOBAL when opening authentication plugins on posix systems. Fixes resolving of symbols in libraries used by authentication plugins.

--- src/lib_load.h
+++ src/lib_load.h
@@ -41,7 +41,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #	define LIB_CLOSE(A) FreeLibrary(A)
 #	define LIB_SYM(HANDLE, SYM) GetProcAddress(HANDLE, SYM)
 #else
-#	define LIB_LOAD(A) dlopen(A, RTLD_NOW)
+#	define LIB_LOAD(A) dlopen(A, RTLD_NOW|RTLD_GLOBAL)
 #	define LIB_CLOSE(A) dlclose(A)
 #	define LIB_SYM(HANDLE, SYM) dlsym(HANDLE, SYM)
 #endif
