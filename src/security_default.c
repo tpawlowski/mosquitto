d97b53cfd53eb54c3e3b09ce5eb720a5656365b8
Roger Light <roger@atchoo.org>
2014-05-26 21:23:19 +0100

    Fix possible leaks.

--- src/security_default.c
+++ src/security_default.c
@@ -142,7 +142,10 @@ int _add_acl(struct mosquitto_db *db, const char *user, const char *topic, int a
 	}
 
 	acl= _mosquitto_malloc(sizeof(struct _mosquitto_acl));
-	if(!acl) return MOSQ_ERR_NOMEM;
+	if(!acl){
+		_mosquitto_free(local_topic);
+		return MOSQ_ERR_NOMEM;
+	}
 	acl->access = access;
 	acl->topic = local_topic;
 	acl->next = NULL;
@@ -188,7 +191,10 @@ int _add_acl_pattern(struct mosquitto_db *db, const char *topic, int access)
 	}
 
 	acl = _mosquitto_malloc(sizeof(struct _mosquitto_acl));
-	if(!acl) return MOSQ_ERR_NOMEM;
+	if(!acl){
+		_mosquitto_free(local_topic);
+		return MOSQ_ERR_NOMEM;
+	}
 	acl->access = access;
 	acl->topic = local_topic;
 	acl->next = NULL;
@@ -309,6 +315,7 @@ int mosquitto_acl_check_default(struct mosquitto_db *db, struct mosquitto *conte
 		if(result){
 			if(access & acl_root->access){
 				/* And access is allowed. */
+				_mosquitto_free(local_acl);
 				return MOSQ_ERR_SUCCESS;
 			}
 		}
